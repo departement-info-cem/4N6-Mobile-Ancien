fd4299cafec9bb8c587130ba49224a85d428d6cb
Maude <maude.sabourin@cegepmontpetit.ca>
2021-02-03 16:12:31 -0500

    Added navDrawerDemo code

---
+++ NavDrawer/NavigationDrawerDemo/app/src/main/java/org/sabourin/navigationdrawerdemo/MainActivity.java
@@ -0,0 +1,90 @@
+package org.sabourin.navigationdrawerdemo;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import org.sabourin.navigationdrawerdemo.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private ActionBarDrawerToggle abToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+        NavigationView nv = binding.navView;
+        DrawerLayout dl = binding.drawerLayout;
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        abToggle = new ActionBarDrawerToggle(this,dl, R.string.drawer_open, R.string.drawer_close){
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(R.string.drawer_open);
+                Toast.makeText(MainActivity.this, "OUVERT", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(R.string.drawer_close);
+                Toast.makeText(MainActivity.this, "FERMÉ", Toast.LENGTH_SHORT).show();
+            }
+        };
+        dl.addDrawerListener(abToggle);
+        abToggle.syncState();
+
+        nv.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Toast.makeText(MainActivity.this,"Texte : " + item.toString(), Toast.LENGTH_SHORT).show();
+                dl.closeDrawers();
+                return false;
+            }
+        });
+
+
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (abToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        abToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(@NonNull Configuration newConfig) {
+        abToggle.onConfigurationChanged(newConfig);
+        super.onConfigurationChanged(newConfig);
+    }
+
+
+}
